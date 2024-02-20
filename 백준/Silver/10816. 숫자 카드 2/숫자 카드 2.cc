4f6b116a82a99527a82ab34dfaad84366b2642d6
coffee-poter <113098614+coffee-poter@users.noreply.github.com>
2024-02-20 10:58:09 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 3852 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int arr[500002];
+    int N, M, card;
+
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> card;
+		arr[i] = card;
+	}
+
+	sort(arr, arr + N);
+
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> card;
+		cout << upper_bound(arr, arr + N, card) - lower_bound(arr, arr + N, card)<<" ";
+	}
+}
