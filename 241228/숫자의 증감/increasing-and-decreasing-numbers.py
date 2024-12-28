96a91115c8a99571c3f085ad39273f6526183054
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2024-12-28 20:09:20 +0900

    Title: 숫자의 증감, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241228/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,9 @@
+k = input().split()
+c, n = k[0], int(k[1])
+
+if c == "A":
+    for i in range(1, n+1):
+        print(i, end=" ")
+elif c == "D":
+    for i in range(n, 0, -1):
+        print(i, end=" ")
