12ee8d84d4a4776d2f9fbba4f753e6e376a449a0
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2024-12-28 09:51:07 +0900

    Title: 코로나 메뉴얼, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241228/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,24 @@
+a = input().split()
+b = input().split()
+c = input().split()
+a1, a2 = a[0], int(a[1])
+b1, b2 = b[0], int(b[1])
+c1, c2 = c[0], int(c[1])
+
+if a1 == "Y" and b1 == "Y":
+    if a2 >= 37 and b2 >= 37:
+        print("E")
+    else:
+        print("N")
+elif a1 == "Y" and c1 == "Y":
+    if a2 >= 37 and c2 >=37:
+        print("E")
+    else:
+        print("N")
+elif b1 == "Y" and c1 == "Y":
+    if b2 >= 37 and c2 >=37:
+        print("E")
+    else:
+        print("N")
+else:
+    print("N")
