ee93fae188edd2863ed3bd33f74cd0001aee633d
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2024-12-28 20:56:16 +0900

    Title: 369 게임, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 241228/369 게임/369-game.py
+++ 241228/369 게임/369-game.py
@@ -1,12 +1,11 @@
 n = int(input())
 
 for i in range(1, n+1):
-    digits = [int(d) for d in str(i)]
-
-    if i < 10 and i % 3 == 0 :
+    if i % 3 == 0:
         print(0, end=" ")
-    elif i > 10 and (digits[0] % 3 == 0 or (digits[1] % 3 == 0 and digits[1] != 0) or i % 3 == 0):
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print(0, end=" ")
+    elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
         print(0, end=" ")
     else:
         print(i, end=" ")
-    
