52de1c920ae9267a67ce2e9e9014f9caedf95451
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2024-12-28 09:54:21 +0900

    Title: 코로나 메뉴얼, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 241228/코로나 메뉴얼/covid-manual.py
+++ 241228/코로나 메뉴얼/covid-manual.py
@@ -5,7 +5,12 @@ a1, a2 = a[0], int(a[1])
 b1, b2 = b[0], int(b[1])
 c1, c2 = c[0], int(c[1])
 
-if a1 == "Y" and b1 == "Y":
+if a1 == "Y" and b1 == "Y" and c1 == "Y":
+    if (a2 >=37 and b2 >= 37) or (a2 >=37 and c2 >= 37) or (b2 >=37 and c2 >= 37):
+        print("E")
+    else:
+        print("N")
+elif a1 == "Y" and b1 == "Y":
     if a2 >= 37 and b2 >= 37:
         print("E")
     else:
@@ -17,11 +22,6 @@ elif a1 == "Y" and c1 == "Y":
         print("N")
 elif b1 == "Y" and c1 == "Y":
     if b2 >= 37 and c2 >=37:
-        print("E")
-    else:
-        print("N")
-elif a1 == "Y" and b1 == "Y" and c1 == "Y":
-    if (a2 >=37 and b2 >= 37) or (a2 >=37 and c2 >= 37) or (b2 >=37 and c2 >= 37):
         print("E")
     else:
         print("N")
