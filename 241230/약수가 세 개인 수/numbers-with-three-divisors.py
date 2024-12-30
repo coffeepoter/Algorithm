519605b69ee2e82ddeade0fe500b373db2dce602
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2024-12-30 23:50:07 +0900

    Title: 약수가 세 개인 수, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241230/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -0,0 +1,12 @@
+n = input().split()
+start, end = int(n[0]), int(n[1])
+res = 0
+
+for i in range(start, end+1):
+    cnt = 0
+    for j in range(i-1,0,-1):
+        if i % j == 0:
+            cnt += 1
+    if cnt == 3:
+        res += 1
+print(res)
