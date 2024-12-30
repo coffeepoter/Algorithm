1fe1156291064397bf08c136a4c87d0e2cbe315e
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2024-12-30 23:45:52 +0900

    Title: 3n + 1 수열 m번, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241230/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -0,0 +1,15 @@
+m = int(input())
+
+for i in range(m):
+    n = int(input())
+    cnt = 0
+    while 1:
+        if n == 1:
+            print(cnt)
+            break
+        if n % 2 == 0:
+            n /= 2
+            cnt += 1
+        else:
+            n = 3 * n + 1
+            cnt += 1
