2ca66843d34048466cf84ca8495f4edc06c1b2fb
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2025-01-02 00:12:41 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 49ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 250102/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
+max_profit = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        profit = (arr[i] - arr[j])*-1
+        if profit > max_profit:
+            max_profit = profit
+
+if max_profit <= 0:
+    print(0)
+else:
+    print(max_profit)
