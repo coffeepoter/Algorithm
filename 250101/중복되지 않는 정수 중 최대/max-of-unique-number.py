12b81a595d537d60136c38d7eafe3f04e6a39974
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2025-01-01 23:24:45 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250101/중복되지 않는 정수 중 최대/max-of-unique-number.py
+++ 250101/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -1,13 +1,15 @@
+MAX_NUM = 1000
+
 n = int(input())
 arr = list(map(int, input().split()))
 
-cnt = [0 for _ in range(n + 1)]
+cnt = [0 for _ in range(MAX_NUM + 1)]
 
 for elem in arr:
     cnt[elem] += 1
 
 ans = -1
-for max_val in range(n, -1, -1):
+for max_val in range(MAX_NUM, -1, -1):
     if cnt[max_val] == 1:
         ans = max_val
         break
