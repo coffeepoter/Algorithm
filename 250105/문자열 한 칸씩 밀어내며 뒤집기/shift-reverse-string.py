c8e0bf1122da9cf948b3315d8e275e8a01076918
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2025-01-05 13:47:16 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 90ms, Memory: 19MB, Status: Runtime Error - CodeTree

--- 250105/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
+++ 250105/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
@@ -2,14 +2,18 @@ s, q = input().split()
 q = int(q)
 for _ in range(q):
     query = int(input())
-    if query == 1:
-        s = s[1:] + s[0]
+    if len(s) > 2:
+        if query == 1:
+            s = s[1:] + s[0]
+            print(s)
+        elif query == 2:
+            s = s[-1] + s[:-1]
+            print(s)
+        elif query == 3:
+            arr = list(s)
+            arr.reverse()
+            print("".join(arr))
+    else:
+        s.reverse()
         print(s)
-    elif query == 2:
-        s = s[-1] + s[:-1]
-        print(s)
-    elif query == 3:
-        arr = list(s)
-        arr.reverse()
-        print("".join(arr))
         
