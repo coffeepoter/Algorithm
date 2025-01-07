eb15db54433938892779f686d1737bfb181b6b38
coffeepoter <113098614+coffeepoter@users.noreply.github.com>
2025-01-07 10:54:42 +0900

    Title: 기수 정렬 구현, Time: 85ms, Memory: 23MB, Status: Passed - CodeTree

---
+++ 250107/기수 정렬 구현/implement-radix-sort.py
@@ -0,0 +1,36 @@
+def counting_sort(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+
+    for i in range(n):
+        index = (arr[i] // exp) % 10
+        count[index] += 1
+    
+    for i in range(1, 10):
+        count[i] += count[i-1]
+    
+    i = n - 1
+    while i >= 0:
+        index = (arr[i] // exp) % 10
+        output[count[index] - 1] = arr[i]
+        count[index] -= 1
+        i -= 1
+    
+    for i in range(n):
+        arr[i] = output[i]
+
+def radix_sort(arr):
+    max_val = max(arr)
+
+    exp = 1
+    while max_val // exp > 0:
+        counting_sort(arr,exp)
+        exp *= 10
+
+n = int(input())
+arr = list(map(int, input().split()))
+radix_sort(arr)
+
+for e in arr:
+    print(e, end=' ')
